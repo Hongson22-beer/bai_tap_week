87840cdfe01bd8f015f4ac2e84d7d6d5f2cfa673
hungnguyen2105 <hungdzs1tg2005@gmail.com>
2026-09-04 22:29:33 +0700

    feat: implement resource roles and role-permission assignment

---
+++ Tuan_01/Week1.Rbac.Api/Models/Role.cs
@@ -0,0 +1,11 @@
+namespace Week1.Rbac.Api.Models;
+
+public sealed class Role
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+
+    public ICollection<RolePermission> RolePermissions { get; set; } = [];
+    public ICollection<UserRole> UserRoles { get; set; } = [];
+}
